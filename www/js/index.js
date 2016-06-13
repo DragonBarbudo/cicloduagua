b369676461f4794eab3f07100f89e7e9cd6678b2
Albin Rdz <ealbinu@gmail.com>
2016-06-13 13:14:48 -0500

    Optimizations 6

--- www/js/index.js
+++ www/js/index.js
@@ -180,11 +180,12 @@ function allReady(){
     $('.botones .btn').click(function(){
         var view = $(this).attr('id');
         view = view.replace('#', '');
+        $('.botones').removeClass('visible');
         $('.info').hide();
         $('.info[data="'+view+'"]').show();
         $('.textbox').addClass('open');
         $('.character').addClass('run');
-        $('.botones').removeClass('visible');
+
         characterInit();
 
     });
@@ -215,7 +216,9 @@ function allReady(){
 }
 
 
-//$(window).load(function() {    loadImage(); });
+
+
+//  $(window).load(function() {    loadImage(); });
 
 
 
